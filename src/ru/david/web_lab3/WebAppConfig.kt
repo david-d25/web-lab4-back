a1b6940d8b3bc17cbf6c94bf5348edd97f7be601
David <david-d25@yandex.ru>
2019-12-21 17:42:13 +0300

    Initial

---
+++ src/ru/david/web_lab3/WebAppConfig.kt
@@ -0,0 +1,80 @@
+package ru.david.web_lab3
+
+import org.hibernate.ejb.HibernatePersistence
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.ComponentScan
+import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.PropertySource
+import org.springframework.core.env.Environment
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories
+import org.springframework.jdbc.datasource.DriverManagerDataSource
+import org.springframework.orm.jpa.JpaTransactionManager
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean
+import org.springframework.transaction.annotation.EnableTransactionManagement
+import org.springframework.web.servlet.config.annotation.EnableWebMvc
+import java.util.*
+import javax.annotation.Resource
+import javax.sql.DataSource
+
+
+@Configuration
+@EnableWebMvc
+@EnableTransactionManagement
+@EnableJpaRepositories("ru.david.web_lab3")
+@PropertySource("classpath:app.properties")
+@ComponentScan("ru.david.web_lab3")
+open class WebAppConfig {
+
+    private val PROP_DATABASE_DRIVER = "db.driver"
+    private val PROP_DATABASE_PASSWORD = "db.password"
+    private val PROP_DATABASE_URL = "db.url"
+    private val PROP_DATABASE_USERNAME = "db.username"
+    private val PROP_HIBERNATE_DIALECT = "db.hibernate.dialect"
+    private val PROP_HIBERNATE_SHOW_SQL = "db.hibernate.show_sql"
+    private val PROP_ENTITYMANAGER_PACKAGES_TO_SCAN = "db.entitymanager.packages_to_scan"
+    private val PROP_HIBERNATE_HBM2DDL_AUTO = "db.hibernate.hbm2ddl.auto"
+
+    @Bean
+    open fun preSalt() = "DJHUYGD7DFG**)ADGHihFhi0F9uFS6tfss^FG4s5F7fsusfhgs9*IF"
+
+    @Bean
+    open fun postSalt() = "IEOHiohG)(hE*(*EGHUG&FV*DFT&DRF$%EDF5ASEFS*(*FHSIORFGH"
+
+    @Resource
+    private val env: Environment? = null
+
+    @Bean
+    open fun dataSource(): DataSource {
+        val dataSource = DriverManagerDataSource()
+        dataSource.setDriverClassName(env!!.getRequiredProperty(PROP_DATABASE_DRIVER))
+        dataSource.url = env.getRequiredProperty(PROP_DATABASE_URL)
+        dataSource.username = env.getRequiredProperty(PROP_DATABASE_USERNAME)
+        dataSource.password = env.getRequiredProperty(PROP_DATABASE_PASSWORD)
+        return dataSource
+    }
+
+    @Bean
+    open fun entityManagerFactory(): LocalContainerEntityManagerFactoryBean? {
+        val entityManagerFactoryBean = LocalContainerEntityManagerFactoryBean()
+        entityManagerFactoryBean.dataSource = dataSource()
+        entityManagerFactoryBean.setPersistenceProviderClass(HibernatePersistence::class.java)
+        entityManagerFactoryBean.setPackagesToScan(env!!.getRequiredProperty(PROP_ENTITYMANAGER_PACKAGES_TO_SCAN))
+        entityManagerFactoryBean.setJpaProperties(getHibernateProperties())
+        return entityManagerFactoryBean
+    }
+
+    @Bean
+    open fun transactionManager(): JpaTransactionManager? {
+        val transactionManager = JpaTransactionManager()
+        transactionManager.entityManagerFactory = entityManagerFactory()!!.getObject()
+        return transactionManager
+    }
+
+    private fun getHibernateProperties(): Properties {
+        val properties = Properties()
+        properties["hibernate.dialect"] = env!!.getRequiredProperty(PROP_HIBERNATE_DIALECT)
+        properties["hibernate.show_sql"] = env.getRequiredProperty(PROP_HIBERNATE_SHOW_SQL)
+        properties["hibernate.hbm2ddl.auto"] = env.getRequiredProperty(PROP_HIBERNATE_HBM2DDL_AUTO)
+        return properties
+    }
+}
