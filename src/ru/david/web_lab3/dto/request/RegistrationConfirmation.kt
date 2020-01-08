7f854ccab3e389258009994e7e98e93fad6f95e6
David <david-d25@yandex.ru>
2020-01-09 02:58:12 +0300

    Code reformatted

--- src/ru/david/web_lab3/dto/request/RegistrationConfirmation.kt
+++ src/ru/david/web_lab3/dto/request/RegistrationConfirmation.kt
@@ -3,7 +3,10 @@ package ru.david.web_lab3.dto.request
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize
 import ru.david.web_lab3.serialization.UrlFriendlyBase64Deserializer
 
-data class RegistrationConfirmation (val email: String, @JsonDeserialize(using = UrlFriendlyBase64Deserializer::class) val token: ByteArray) {
+data class RegistrationConfirmation (
+        val email: String,
+        @JsonDeserialize(using = UrlFriendlyBase64Deserializer::class) val token: ByteArray
+) {
     override fun equals(other: Any?): Boolean {
         if (this === other) return true
         if (javaClass != other?.javaClass) return false
